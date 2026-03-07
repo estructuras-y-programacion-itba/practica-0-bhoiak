e0d7414ade6c861f5fe17f03f6d4b83884d1c227
Bautista <bhoiak@itba.edu.ar>
2026-03-07 18:16:21 -0300

    tirada de dados de un jugador

--- main.py
+++ main.py
@@ -3,12 +3,6 @@
 def hola_mundo():
     return "hola_mundo"
 
-
-def main():
-    # Aqui ejecutas tus soluciones
-    print(hola_mundo())
-
-
 def main():
     # Aqui ejecutas tus soluciones
     print(hola_mundo())
@@ -31,78 +25,95 @@ def tirar_un_dado():
     return dado
 
 
-def nuevos_dados(dado_elegido, tirada1):
+def nuevos_dados(tirada1, cambio_dados):
 
-    if dado_elegido not in ["1","2","3","4","5"]:
+    dados_elegidos = []
+    contar = 0
+    while contar < int(cambio_dados):
+            dado_elegido = (input("Que dado quiere tirar nuevamente? 1 para el 1ro, 2 para el segundo... etc"))
+        
+            if dado_elegido not in ["1","2","3","4","5"]:
                 print("Listo, sus dados son: ", tirada1) 
                 return tirada1
-
-    else: 
-          dado_elegido = int(dado_elegido)
-          indice = dado_elegido -1
-          tirada1[indice] = tirar_un_dado()   
+            
+            elif dado_elegido in dados_elegidos:
+                print("Dado repetido, elija otro.")
+            
+            else:
+                dado_elegido = int(dado_elegido)
+                dados_elegidos.append(str(dado_elegido))
+                indice = dado_elegido -1
+                tirada1[indice] = tirar_un_dado()
+                contar += 1
 
     return tirada1
 
 
 def generala():
     tirada1 = tirar()
+    nuevo_tiro = tirada1
     generala = True
-    n = 0
+    cant_tiros = 0
 
-    for i in tirada1:
-        if i == tirada1[0]:
-            n = n+1
-            if n != 5:   
-                generala = False
+    while cant_tiros < 2:
+        n = 0
+        m = 0
+        for i in nuevo_tiro:
+            m = m+ 1
 
-        else:
-            print("GENERALA!")
-            return
-    
-    if generala == False:
-        cambio_dados = input(("Cuantos dados quiere tirar nuevamente? : "))
-        if cambio_dados not in ["1","2","3","4","5"]:
-            print("Listo, sus dados son: ", tirada1) 
-            return
+            if i == nuevo_tiro[0]:
+                n = n+1
         
-        else:
-
-            contar = 0    
-            while contar < int(cambio_dados):
-                dado_elegido = input(("Que dado quiere cambiar? (poner 1 o 2 o 3... segun el dado que quiera tirar) : "))
-                if dado_elegido not in ["1","2","3","4","5"]:
-                    print("Listo, sus dados son: ", tirada1) 
+            if n == 5:   
+                generala = True
+                print("GENERALA!")
+                return
+            elif n!= 5 and m == 5:
+                generala = False
+                cambio_dados = input(("Cuantos dados quiere tirar nuevamente? : "))
+                if cambio_dados not in ["1","2","3","4","5"]:
+                    print("Listo, sus dados son: ", nuevo_tiro) 
                     return
-
-                if dado_elegido == "1":
-                    nuevo_tiro = nuevos_dados(dado_elegido, tirada1)
-                    print("Su nuevos dados son: ", nuevo_tiro)
-                    contar += 1
                 
-                if dado_elegido == "2":
-                    nuevo_tiro = nuevos_dados(dado_elegido, tirada1)
+                else:
+                    nuevo_tiro = nuevos_dados(nuevo_tiro, cambio_dados)
                     print("Su nuevos dados son: ", nuevo_tiro)
-                    contar += 1
-
-                if dado_elegido == "3":
-                    nuevo_tiro = nuevos_dados(dado_elegido, tirada1)
-                    print("Su nuevos dados son: ", nuevo_tiro)
-                    contar += 1
-
-                if dado_elegido == "4":
-                    nuevo_tiro = nuevos_dados(dado_elegido, tirada1)
-                    print("Su nuevos dados son: ", nuevo_tiro)
-                    contar += 1
-
-                elif dado_elegido == "5":
-                    nuevo_tiro = nuevos_dados(dado_elegido, tirada1)
-                    print("Su nuevos dados son: ", nuevo_tiro)
-                    contar += 1
-    #en realidad deberia meter el contar en la otra funcion para poder usarla cada vez que quiera hacer un nuevo tiro, y ese nuevo tiro gruardarlo en mi lista tirada1.                
-        return nuevo_tiro
+                    cant_tiros += 1
+        
+    return nuevo_tiro
     
+jugar = generala()
+
 
 # No cambiar a partir de aqui
 if __name__ == "__main__":
     main()
+
+                
+    
+                
+"""   
+while cant_tiros < 2:
+        n = 0
+        m = 0
+        for i in nuevo_tiro:
+            m = m+ 1
+            if i == nuevo_tiro[0]:
+                n = n+1
+        
+            if n == 5:   
+                generala = True
+                print("GENERALA!")
+                return
+            elif n!= 5 and m == 5:
+                generala = False
+                cambio_dados = input(("Cuantos dados quiere tirar nuevamente? : "))
+                if cambio_dados not in ["1","2","3","4","5"]:
+                    print("Listo, sus dados son: ", nuevo_tiro) 
+                    return
+                
+                else:
+                    nuevo_tiro = nuevos_dados(nuevo_tiro, cambio_dados)
+                    print("Su nuevos dados son: ", nuevo_tiro)
+                    cant_tiros += 1
+"""
